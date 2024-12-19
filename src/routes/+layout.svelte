d8961bb4c03836dcecb618257b70824736914faa
Seijno <seijnovannieuwkoop@gmail.com>
2024-12-19 22:37:01 +0100

    verkeerde layout gecopied

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
   import { Header, Footer } from '$lib/index'
   export let data
+  import { onNavigate } from '$app/navigation';
+
+onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+        document.startViewTransition(async () => {
+            resolve();
+            await navigation.complete;
+        });
+    });
+});
 </script>
 
 <Header navigation={data.navigation} />
@@ -10,3 +22,17 @@
 </main>
 
 <Footer footerItems={data.footer} />
+
+<style>
+@view-transition {
+  navigation: auto;
+}
+
+::view-transition-group(name){
+  animation: animatie 5s;
+}
+@keyframes animatie { 
+0% { transform: translate(100%, 0%)}  50%{ transform: translate( 0%, 0%)}
+0% { transform: translate(100%, 0%)}
+}
+</style>
